4b3df5a1fb3dc82d16cc377109e7a656489f17c0
VICKY2501 <vaibhav_se20b9_39@dtu.ac.in>
2024-01-03 19:47:19 +0530

    First Commit,Time: 07:46 P.M.

---
+++ app/src/main/java/com/example/speerai/FollowerFollowingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.speerai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FollowerFollowingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_follower_following)
+    }
+}
